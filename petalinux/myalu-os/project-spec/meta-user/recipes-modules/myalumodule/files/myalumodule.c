99a9f8725521ff86ba0cb58c624ee48477245cf8
Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>
2022-12-04 20:03:24 -0300

    Unable to handle kernel paging

--- petalinux/myalu-os/project-spec/meta-user/recipes-modules/myalumodule/files/myalumodule.c
+++ petalinux/myalu-os/project-spec/meta-user/recipes-modules/myalumodule/files/myalumodule.c
@@ -13,9 +13,13 @@ dev_t dev = 0;
 static struct class *dev_class;
 static struct cdev myalu_cdev;
 
-/*
-** Function Prototypes
-*/
+void __iomem *r0 = (void *)0x43C00000;
+void __iomem *r1 = (void *)0x43C00004;
+void __iomem *r2 = (void *)0x43C00008;
+void __iomem *r3 = (void *)0x43C0000C;
+void __iomem *r4 = (void *)0x43C00010;
+void __iomem *r5 = (void *)0x43C00014;
+
 static int __init myalu_driver_init(void);
 static void __exit myalu_driver_exit(void);
 static int myalu_open(struct inode *inode, struct file *file);
@@ -32,39 +36,47 @@ static struct file_operations fops =
 		.release = myalu_release,
 };
 
-/*
-** This function will be called when we open the Device file
-*/
 static int myalu_open(struct inode *inode, struct file *file)
 {
-	pr_info("Driver Open Function Called...!!!\n");
-	// TODO: informar si el dispositivo está ready for data
+	u32 ready = ioread32(r1);
+	ready = ready >> 1;
+	if (0x00000000 != ready)
+	{
+		pr_info("myalu IPCORE ready for work.\n");
+	}
+	else
+	{
+		pr_info("myalu IPCORE is bussy.\n");
+	}
 	return 0;
 }
 
-/*
-** This function will be called when we close the Device file
-*/
 static int myalu_release(struct inode *inode, struct file *file)
 {
-	pr_info("Driver Release Function Called...!!!\n");
-	// TODO: informar si el dispositivo está ready for data
+	u32 ready = ioread32(r1);
+	ready = ready >> 1;
+	if (0x00000000 != ready)
+	{
+		pr_info("myalu IPCORE ready for work.\n");
+	}
+	else
+	{
+		pr_info("myalu IPCORE is bussy.\n");
+	}
 	return 0;
 }
 
-/*
-** This function will be called when we read the Device file
-*/
 static ssize_t myalu_read(struct file *filp, char __user *buf, size_t len, loff_t *off)
 {
-	pr_info("Driver Read Function Called...!!!\n");
-	// TODO: leer resultado y carry
+	wmb();
+	u32 result = ioread32(r5);
+	wmb();
+	u32 carry = ioread32(r1);
+	carry = carry & (0x00000001);
+	pr_info("%d,%d\n", carry, result);
 	return 0;
 }
 
-/*
-** This function will be called when we write the Device file
-*/
 static ssize_t myalu_write(struct file *filp, const char __user *buf, size_t len, loff_t *off)
 {
 	pr_info("Driver Write Function Called...!!!\n");
@@ -72,9 +84,6 @@ static ssize_t myalu_write(struct file *filp, const char __user *buf, size_t len
 	return len;
 }
 
-/*
-** Module Init function
-*/
 static int __init myalu_driver_init(void)
 {
 	/*Allocating Major number*/
@@ -108,9 +117,8 @@ static int __init myalu_driver_init(void)
 		pr_err("Cannot create the Device 1\n");
 		goto r_device;
 	}
-	pr_info("Device Driver Insert...Done!!!\n");
-
-	// TODO: habilitar el ipcore
+	pr_info("starting myalu IPCORE.\n");
+	iowrite32(0x00000001, r0);
 	return 0;
 
 r_device:
@@ -120,17 +128,14 @@ r_class:
 	return -1;
 }
 
-/*
-** Module exit function
-*/
 static void __exit myalu_driver_exit(void)
 {
 	device_destroy(dev_class, dev);
 	class_destroy(dev_class);
 	cdev_del(&myalu_cdev);
 	unregister_chrdev_region(dev, 1);
-	pr_info("Device Driver Remove...Done!!!\n");
-	// TODO: deshabilitar el ipcore
+	pr_info("stoping myalu IPCORE\n");
+	iowrite32(0x00000002, r0);
 }
 
 module_init(myalu_driver_init);
@@ -138,5 +143,5 @@ module_exit(myalu_driver_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>");
-MODULE_DESCRIPTION("myalumodule - module to communicate with myalu ipcore.");
+MODULE_DESCRIPTION("myalumodule - module to communicate with myalu IPCORE.");
 MODULE_VERSION("1:0.0");
